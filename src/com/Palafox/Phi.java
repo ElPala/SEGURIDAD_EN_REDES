884f92d036e93c11c2f432fedb6c77b895485f12
Elpala <luis.pala1997@gmail.com>
2019-10-01 00:36:18 -0500

    updating phi algorithm

--- src/com/Palafox/Phi.java
+++ src/com/Palafox/Phi.java
@@ -6,10 +6,24 @@ public class Phi {
 
     public static void main(String[] args) {
         BigInteger x = new BigInteger("2199055761527");
-        for (BigInteger i = new BigInteger("1");!i.equals(x); i = i.add(BigInteger.ONE)){
+        BigInteger y;
+        BigInteger menosuno =  BigInteger.ONE;
+        menosuno =  menosuno.negate();
+        for (BigInteger i = new BigInteger("2");!i.equals(x); i = i.add(BigInteger.ONE)){
             if((x.mod(i)).equals(BigInteger.ZERO)){
-                System.out.println(""+i);
-                x = x.divide(i);
+                System.out.printf("("+i);
+                y = x.divide(i);
+                System.out.printf(")("+y+") = " + x);
+
+                i = i.add(menosuno);
+                y = y.add(menosuno);
+                System.out.println();
+                System.out.printf("("+i + ")(" +y+") = ");
+                i = i.multiply(y);
+
+
+                System.out.println("PHI = " + i);
+                return;
             }
         }
     }
